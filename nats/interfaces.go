11f7829d61a5ea8d94a0b4ee401137091c7afa30
DKhorkov <dkhorkov.dev@gmail.com>
2025-04-09 20:02:31 +0300

    created mocks for nats broker

--- nats/interfaces.go
+++ nats/interfaces.go
@@ -1,12 +1,16 @@
 package nats
 
 // Worker asynchronously processes NATS messages in goroutines.
+//
+//go:generate mockgen -source=interfaces.go -destination=mocks/worker.go -package=mocks -exclude_interfaces=Publisher
 type Worker interface {
 	Run() error
 	Stop() error
 }
 
 // Publisher publishes messages to NATS broker.
+//
+//go:generate mockgen -source=interfaces.go -destination=mocks/publisher.go -package=mocks -exclude_interfaces=Worker
 type Publisher interface {
 	Publish(subject string, content []byte) error
 	Close() error
